63d19d37533be01ce6e316aa1a255f6949bc7b6c
Alexandre Konioukhov <alexan1@gmail.com>
2018-05-07 10:14:33 -0500

    Add model, controler and dbcontext

---
+++ PeopleApi/Data/PeopleContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PeopleApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleApi.Data
+{
+    public class PeopleDbContext : DbContext
+    {
+        public PeopleDbContext(DbContextOptions<PeopleDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Rating>()
+        .HasKey(a => new { a.PersonID, a.UserID });
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Rating> Rating { get; set; }
+    }
+}
---
+++ PeopleApi/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleApi.Models
+{
+    public class Rating
+    {
+        public int PersonID { get; set; }
+        public string UserID { get; set; }
+        public int Rate { get; set; }
+    }
+}
