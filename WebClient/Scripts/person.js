e8340cc146b8cfea8510aadc4d6cfb2a00b6bd54
Alexandre Konioukhov <alexan1@gmail.com>
2018-06-10 19:50:25 -0400

    getPerson3

--- WebClient/Scripts/person.js
+++ WebClient/Scripts/person.js
@@ -34,3 +34,23 @@ function getPerson2() {
         .then(response => response.json())
         .then(data => console.log(data));
 }
+
+function getPerson3() {
+
+    var url = "https://query.wikidata.org/sparql";
+
+    var query = 'SELECT DISTINCT  ?item ?itemLabel ?itemDescription (SAMPLE(?DR) AS ?DRSample) (SAMPLE(?article) AS ?articleSample)'
+        + 'WHERE{ ?article  schema:about       ?item ; schema:inLanguage  "en" ; schema:isPartOf    <https://en.wikipedia.org/>'
+        + 'FILTER ( ?item = <http://www.wikidata.org/entity/Q937> )'
+        + 'OPTIONAL { ?item  wdt:P569  ?DR }'
+        + 'OPTIONAL { ?item  wdt:P570  ?RIP }'
+        + 'OPTIONAL { ?item  wdt:P18  ?image }'
+        + 'SERVICE wikibase:label { bd:serviceParam wikibase:language  "en"}}'
+        + 'GROUP BY ?item ?itemLabel ?itemDescription';
+
+    var queryUrl = encodeURI(url + '?query=' + query + '&format=json');
+
+    fetch(queryUrl)
+        .then(wdk.simplifySparqlResults)
+        .then(simplifiedResults => console.log(simplifiedResults));
+}
