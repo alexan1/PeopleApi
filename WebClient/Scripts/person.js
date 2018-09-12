da3725892a658b98e3ec33a7c4454ff333771c5a
Alexandre Konioukhov <alexan1@gmail.com>
2018-09-11 23:47:28 -0400

    getperson

--- WebClient/Scripts/person.js
+++ WebClient/Scripts/person.js
@@ -6,22 +6,13 @@
 ko.applyBindings(new ViewModel());
 
 
-function getPerson1() {    
-
-    var query1 = 'https://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q937&format=json';
-
-    fetch(query1)
-        .then(response => response.json())
-        .then(data => console.log(data));    
-}
-
-function getPerson2() {
+function getPerson(qid) {
 
     var url = "https://query.wikidata.org/sparql";
 
     var query = 'SELECT DISTINCT  ?item ?itemLabel ?itemDescription (SAMPLE(?DR) AS ?DRSample) (SAMPLE(?article) AS ?articleSample)'
         + 'WHERE{ ?article  schema:about       ?item ; schema:inLanguage  "en" ; schema:isPartOf    <https://en.wikipedia.org/>'
-        + 'FILTER ( ?item = <http://www.wikidata.org/entity/Q937> )'
+        + 'FILTER ( ?item = <http://www.wikidata.org/entity/Q' + qid + '> )'
         + 'OPTIONAL { ?item  wdt:P569  ?DR }'
         + 'OPTIONAL { ?item  wdt:P570  ?RIP }'
         + 'OPTIONAL { ?item  wdt:P18  ?image }'
@@ -34,24 +25,4 @@ function getPerson2() {
         .then(response => response.json())        
         .then(data => data.results.bindings[0])
         .then(data => console.table(data));
-}
-
-function getPerson3() {
-
-    var url = "https://query.wikidata.org/sparql";
-
-    var query = 'SELECT DISTINCT  ?item ?itemLabel ?itemDescription (SAMPLE(?DR) AS ?DRSample) (SAMPLE(?article) AS ?articleSample)'
-        + 'WHERE{ ?article  schema:about       ?item ; schema:inLanguage  "en" ; schema:isPartOf    <https://en.wikipedia.org/>'
-        + 'FILTER ( ?item = <http://www.wikidata.org/entity/Q937> )'
-        + 'OPTIONAL { ?item  wdt:P569  ?DR }'
-        + 'OPTIONAL { ?item  wdt:P570  ?RIP }'
-        + 'OPTIONAL { ?item  wdt:P18  ?image }'
-        + 'SERVICE wikibase:label { bd:serviceParam wikibase:language  "en"}}'
-        + 'GROUP BY ?item ?itemLabel ?itemDescription';
-
-    var queryUrl = encodeURI(url + '?query=' + query + '&format=json');
-
-    fetch(queryUrl)
-        .then(wdk.simplifySparqlResults)
-        .then(simplifiedResults => console.log(simplifiedResults));
 }
