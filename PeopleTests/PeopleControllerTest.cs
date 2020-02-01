b8f1bf390b680b5945f01d91fbdffb726fd50edf
Alexandre Konioukhov <alexan1@gmail.com>
2020-02-01 00:00:43 -0500

    GetPeopleTest

--- PeopleTests/PeopleControllerTest.cs
+++ PeopleTests/PeopleControllerTest.cs
@@ -4,6 +4,7 @@ using PeopleApi.Data;
 using PeopleApi.Models;
 using System.Linq;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace PeopleTests
 {
@@ -19,15 +20,27 @@ namespace PeopleTests
 
             using (var context = new PeopleDbContext(options))
             {
-                context.Person.Add(new Person { ID = 1, Name = "Putin", Rate = null });
-                context.SaveChanges();
+                var person1 = new Person { ID = 1, Name = "Putin", Rate = null };
+                var person2 = new Person { ID = 2, Name = "Trump", Rate = null };                
 
                 var controller = new PeopleApi.Controllers.PeopleController(context);
+
+                var action1 = controller.PostPerson(person1);
+                Assert.AreEqual(TaskStatus.RanToCompletion, action1.Status);
+                var action2 = controller.PostPerson(person2);
+                Assert.AreEqual(TaskStatus.RanToCompletion, action2.Status);
+
                 var result = controller.GetPeople();
                 var count = result.Count();
 
                 Assert.IsNotNull(result);
-                Assert.AreEqual(1, count);
+                Assert.AreEqual(2, count);
+
+                var action3 = controller.DeletePerson(1);
+                Assert.AreEqual(TaskStatus.RanToCompletion, action3.Status);
+                var action4 = controller.DeletePerson(2);
+                Assert.AreEqual(TaskStatus.RanToCompletion, action4.Status);
+
 
             }
         }
